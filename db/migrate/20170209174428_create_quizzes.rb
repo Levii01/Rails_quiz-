a2525901afbc8c620121fa784bd9e2fbeaccf39c
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-02-09 18:59:02 +0100

    Add Quizz model

---
+++ db/migrate/20170209174428_create_quizzes.rb
@@ -0,0 +1,10 @@
+class CreateQuizzes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :quizzes do |t|
+      t.string :title, null: false
+      t.string :description, null: false
+
+      t.timestamps
+    end
+  end
+end
