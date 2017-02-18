717ef36447489a4e82b24d71bfd69fe388de9665
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-02-18 03:31:54 +0100

    Add routes to question and answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :quizzes
+  resources :questions
+  resources :answers
+
   get 'welcome/index'
-  
+
   root to: 'welcome#index'
 end
